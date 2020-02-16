640b3e9735c44dfce8a1172dfcedb3e1a2fe946c
Ari <ari.blondal@gmail.com>
2020-02-16 10:44:48 -0800

    woot woot

---
+++ scripts/wolf_to_save.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var colour = "white"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite.animation = colour
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Wolf":
+		body.has_colours.append(colour)
